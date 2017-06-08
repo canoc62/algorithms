3acbc8d017db7b55f6f2b590f9f1e6b5d4193b9e
Christopher Cano <canoc4262@gmail.com>
2017-06-07 23:00:05 -0700

    Add problem description and bst node definition to recover-bst.

--- js/recursion/recover-bst.js
+++ js/recursion/recover-bst.js
@@ -1,14 +1,20 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
+Two elements of a binary search tree (BST) are swapped by mistake.
+Recover the tree without changing its structure.
+
+Note:
+A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
  */
 /**
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
  */
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
 var recoverTree = function(root) {
     
 };
