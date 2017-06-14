6cf563afbb40b1c5b8b0e718a834877904fc022a
Christopher Cano <canoc4262@gmail.com>
2017-06-14 01:36:28 -0700

    Fix maxTwo logic.

--- java/general/maxTwo.java
+++ java/general/maxTwo.java
@@ -1,55 +1,67 @@
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Main {
-  
-  public static void main(String[] args) {
-    ArrayList<int> numbers = new ArrayList<int>();
-    numbers.add(4, 4, 3, 2, 1);
-    
-    ArrayList<int> numbers2 = new ArrayList<int>();
-    numbers.add(4, 4, 7, 2, 5);
-    
-    MaxTwo getResult1 = new MaxTwo(numbers);
-    System.out.println("Result 1: " + getResult1.getResults());
-    MaxTwo getResult2 = new MaxTwo(numbers2);
-    System.out.println("Result 2: " + getResult2.getResults());
-  }
-  
-  private class MaxTwo {
-    
-    private ArrayList<int> numbers;
-    private largest;
-    private secondLargest;
-    
-    public MaxTwo(ArrayList<int> numbers) {
-      this.numbers = numbers;
+public class MaxTwo {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(4);
+        numbers.add(4);
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(1);
+
+        ArrayList<Integer> numbers2 = new ArrayList<Integer>();
+        numbers2.add(4);
+        numbers2.add(4);
+        numbers2.add(7);
+        numbers2.add(2);
+        numbers2.add(5);
+
+        MaxTwoInner getResult1 = new MaxTwoInner(numbers);
+        System.out.println("Result 1: " + Arrays.toString(getResult1.getResults()));
+        MaxTwoInner getResult2 = new MaxTwoInner(numbers2);
+        System.out.println("Result 2: " + Arrays.toString(getResult2.getResults()));
     }
-    
-    public int[] getResults(ArrayList<int> numbers) {
-      if (numbers.size() < 2) {
-        return -1;
-      }
-      
-      int firstVal = numbers.get(0);
-      int secondVal = numbers.get(1);
-      
-      int largest = firstVal > secondVal ? firstVal : secondVal;
-      int secondLargest = firstVal > secondVal ? secondVal : firstVal;
-      
-      for (int i = 2; i < numbers.size(); i++) {
-        if (numbers.get(i) > largest) {
-          secondLargest = largest;
-          largest = numbers.get(i);
-        } else if (numbers.get(i) > secondLargest) {
-          secondLargest = numbers.get(i);
+
+    private static class MaxTwoInner {
+
+        private ArrayList<Integer> numbers;
+        private int largest;
+        private int secondLargest;
+
+        public MaxTwoInner(ArrayList<Integer> numbers) {
+            this.numbers = numbers;
         }
-      }
-      
-      int[] result = new int[2];
-      result = [largest, secondLargest];
-      
-      return result;
+
+        public int[] getResults() {
+            int[] result = new int[2];
+            if (this.numbers.size() < 2) {
+                return result;
+            }
+
+            int firstVal = this.numbers.get(0);
+            int secondVal = this.numbers.get(1);
+
+            int largest = firstVal > secondVal ? firstVal : secondVal;
+            int secondLargest = firstVal > secondVal ? secondVal : firstVal;
+
+            for (int i = 2; i < this.numbers.size(); i++) {
+                if (this.numbers.get(i) > largest) {
+                    secondLargest = largest;
+                    largest = this.numbers.get(i);
+                } else if (this.numbers.get(i) > secondLargest) {
+                    secondLargest = this.numbers.get(i);
+                }
+            }
+            
+            result[0] = largest;
+            result[1] = secondLargest;
+            return result;
+        }
+
+        // public String toString() {
+        //   return "" + this.getResults().toString();
+        // }
     }
-  }
 }
