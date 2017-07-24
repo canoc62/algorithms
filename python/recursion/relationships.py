548c73056d4c532b1e330fd6cd37969f77213ce3
Christopher Cano <canoc4262@gmail.com>
2017-07-24 01:11:48 -0700

    Finish relationships recursion problem in python.

--- python/recursion/relationships.py
+++ python/recursion/relationships.py
@@ -12,3 +12,36 @@
 # e.g. with the lists above as input, with input names 'Bart' and 'Homer', you should return:
 #    ['Bart son Homer', 'Bart brother Lisa daughter Homer']
 #
+
+def getRelationships(relList, name1, name2):
+  result = []
+  originalName = name1
+
+  def scan(relList, name1, name2, relationship = ''):
+    if relationship != '':
+      relationship += ' '
+
+    for i in range(len(relList)):
+      currentRelationship = relList[i]
+      if currentRelationship[0] == name1:
+        relationship += currentRelationship[0] + ' ' + currentRelationship[1]
+        if currentRelationship[2] == name2:
+          relationship += ' ' + currentRelationship[2]
+          result.append(relationship)
+          if name1 != originalName:
+            return
+        else:
+          scan(relList, currentRelationship[2], name2, relationship)
+          relationship = ''
+    return result
+
+  return scan(relList, name1, name2)
+
+
+rels = [['Bart', 'brother', 'Lisa'], ['Bart', 'son', 'Homer'],\
+       ['Marge', 'wife', 'Homer'], ['Lisa', 'daughter', 'Homer']]
+
+print(getRelationships(rels, 'Bart', 'Homer'))
+
+  
+   
